eec43b5525085fd90b115855ce614b5fdabf0fc8
Juan Mateo FRASINELLI <d46710280@alumnos.itr.edu.ar>
2024-07-24 11:25:25 -0300

    Added the filter

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -3,29 +3,32 @@ import React, {useState} from "react";
 
 function TodoList() {
   const categories = ["Estudio", "Hogar", "Salud", "Ocio", "Otro"]
+  const [currentCategory, setCurrentCategory] = useState("Todas");
   const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState({text: "", category:categories[0]});
+  const [newTask, setNewTask] = useState({text: "", category:categories[0], id: 0});
 
 
   const handleInputChange = (e) => {
     e.preventDefault();
 
-    if (e.target.id == "task") setNewTask({...newTask , text: e.target.value});
+    if (e.target.id === "task") setNewTask({...newTask , text: e.target.value});
     
-    if (e.target.id == "category") setNewTask({...newTask , category: e.target.value});
+    if (e.target.id === "category") setNewTask({...newTask , category: e.target.value});
+    
+    if (e.target.id === "filter") setCurrentCategory(e.target.value);
   }
 
   const addTask = (e) => {
     e.preventDefault()
     if (newTask.text.trim() !== "") {
       setTasks([...tasks, newTask]);
-      
+      console.log(newTask)
     }
-    setNewTask({text: "", category: categories[0]});
+    setNewTask({text: "", category: newTask.category, id: newTask.id + 1});
   };
 
   const removeTask = (index) => {
-    const newList = tasks.filter(value => value !== tasks[index])
+    const newList = tasks.filter(value => value.id !== index)
     setTasks(newList)
   }
   
@@ -34,11 +37,23 @@ function TodoList() {
       
       <form onSubmit={addTask}>
         <h2>Lista de Tareas</h2>
+        <select value={currentCategory} id="filter" onChange={handleInputChange}>
+          {categories.map((value) => <option>{value}</option>)}
+          <option>Todas</option>
+        </select>
         <ul>
-          {tasks.map((object, index) => (<div class="container">
-            <li key={index}>{object.text} <p>{object.category}</p></li>
-            <button onClick={() => removeTask(index)}>Eliminar</button>
-          </div>))}
+          
+          {currentCategory === "Todas"
+          ? tasks.map((object) => 
+            (<div class="container">
+              <li key={object.id}>{object.text} <p>{object.category}</p></li>
+              <button onClick={() => removeTask(object.id)}>Eliminar</button>
+            </div>)) 
+          : tasks.filter(value => value.category == currentCategory).map((object) => 
+            (<div class="container">
+              <li key={object.id}>{object.text} <p>{object.category}</p></li>
+              <button onClick={() => removeTask(object.id)}>Eliminar</button>
+            </div>))}
         </ul>
         <p>Nueva tarea:</p>
         <input value={newTask.text} id="task" type="text" onChange={handleInputChange}></input>
